f030135dd53cb3bbb571720654c7c8da1a4a2c15
Virendra Maurya <mvirendra115@gmail.com>
2021-05-03 20:30:36 +0530

    First Android App

---
+++ app/src/main/java/com/example/chatapp/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+       new Handler().postDelayed(() -> startActivity(new Intent(SplashActivity.this,PhoneNumberActivity.class)),5000);
+    }
+}
